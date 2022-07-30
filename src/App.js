5f1fdec663c5fcb21f0f4efbfa42acd175bc9b12
samir281 <samalam281@gmail.com>
2022-07-31 01:55:38 +0530

    problem in registration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./component/Home";
+import Registration from './component/Registration';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        hello
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home register="false" />} />
+        <Route path="/register" element={<Registration />} />
+      </Routes>
+    </Router>
+  </div>
+ );
 }
 
 export default App;
---
+++ src/component/Registration.js
@@ -0,0 +1,116 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import Header from "./Header";
+import "../styles/Registration.css"
+import { Form, Button, Container } from 'react-bootstrap';
+import { url } from './url'
+
+export default function Registration() {
+    let navigate = useNavigate();
+    const [errors, setErrors] = useState({})
+    const [form, setForm] = useState({})
+
+    const setField = (field, value) => {
+        setForm({
+            ...form,
+            [field]: value
+        })
+        if (!!errors[field]) setErrors({
+            ...errors,
+            [field]: null
+        })
+    }
+
+    const handleSubmit = (data) => {
+        data.preventDefault();
+        const newErrors = findFormErrors()
+        console.log(newErrors)
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors)
+        } else {
+            let customerId = Math.floor(Math.random() * (999 - 100 + 1) + 100);
+
+            console.log(form.firstName)
+            console.log(JSON.stringify(form))
+            fetch(`${url}/signup`, {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(form)
+            }).then(response => {
+                console.log(response)
+              })
+              .catch(error => {
+                console.log(error)
+              })
+        }
+    }
+
+    const findFormErrors = () => {
+        const { firstName, lastName, password, email,phoneNumber} = form
+        const newErrors = {}
+        const emailregex = /^([\w.%+-]+)@([\w-]+\.)+([\w]{1,})$/i;
+        const nameregex = /^[aA-zZ\s]*$/
+        if (!firstName || firstName === '') newErrors.firstName = 'cannot be blank!'
+        else if (nameregex.test(firstName) === false) newErrors.firstName = 'can contain only letters and spaces'
+        if (!lastName || lastName === '') newErrors.lastName = 'cannot be blank!'
+        else if (nameregex.test(lastName) === false) newErrors.lastName = 'can contain only letters and spaces'
+        if (emailregex.test(email) === false) newErrors.email = "invalid email(should contain a '.' & '@')"
+        if (!password || password === '') newErrors.password = 'cannot be blank'
+        //if(phoneNumber) newErrors.phoneNumber='cannot be blank'
+        return newErrors
+    }
+
+    return (
+        <div>
+            <Header />
+            <Container>
+                <h3 className="Reg-Header">User Registration</h3>
+
+                <Form.Group className="mb-3">
+                    <Form.Label id="fname">First Name:</Form.Label>
+                    <Form.Control type="text" aria-labelledby="fname" name="fname" placeholder="Firstname" onChange={e => setField('firstName', e.target.value)} isInvalid={!!errors.firstName} />
+                    <Form.Control.Feedback type='invalid' data-testid="nameerr">
+                        {errors.firstName}
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label id="lname">Last Name:</Form.Label>
+                    <Form.Control type="text" aria-labelledby="lname" name="lname" placeholder="Lastname" onChange={e => setField('lastName', e.target.value)} isInvalid={!!errors.lastName} />
+                    <Form.Control.Feedback type='invalid' data-testid="nameerr">
+                        {errors.lastName}
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label id="email">Email/UserName</Form.Label><br />
+                    <Form.Control type="text" aria-labelledby="email" data-testid="email" name="email" placeholder="Email" onChange={e => setField('email', e.target.value)} isInvalid={!!errors.email} />
+                    <Form.Control.Feedback type='invalid' data-testid="nameerr">
+                        {errors.email}
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+
+                <Form.Group className="mb-3">
+                    <Form.Label id="password">Password</Form.Label><br />
+                    <Form.Control type="password" aria-labelledby="password" data-testid="password" name="password" placeholder="Password" onChange={e => setField('password', e.target.value)} isInvalid={!!errors.password} />
+                    <Form.Control.Feedback type='invalid' data-testid="nameerr">
+                        {errors.password}
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label id="phoneNumber">Phone Number</Form.Label><br />
+                    <Form.Control type="text" aria-labelledby="phoneNumber" data-testid="phoneNumber" name="phoneNumber" placeholder="phoneNumber" onChange={e => setField('phoneNumber', e.target.value)} isInvalid={!!errors.phoneNumber} />
+                    <Form.Control.Feedback type='invalid' data-testid="nameerr">
+                        {errors.phoneNumber}
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                <div className="regButton">
+                    <Button type='submit' onClick={handleSubmit}>Register</Button>
+                </div>
+            </Container>
+        </div>
+    )
+}
