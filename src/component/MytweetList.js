3daf52262f9a8a39a00cefa42159fab1bc586512
samir281 <samalam281@gmail.com>
2022-08-09 14:22:02 +0530

    Like implemented

--- src/component/MytweetList.js
+++ src/component/MytweetList.js
@@ -1,135 +1,209 @@
-import {   FaUserSecret} from "react-icons/fa";
+import { FaUserSecret } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
-import { Container, Card,Row, Col,Modal, Table, ModalHeader, ModalBody,ModalFooter} from "react-bootstrap";
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Box from '@mui/material/Box';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import CommentRoundedIcon from '@mui/icons-material/CommentRounded';
-import Fab from '@mui/material/Fab';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
-import { useState } from "react";
+import {
+  Container,
+  Card,
+  Row,
+  Col,
+  Modal,
+  Table,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "react-bootstrap";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Box from "@mui/material/Box";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import Fab from "@mui/material/Fab";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import CloseFullscreenIcon from "@mui/icons-material/CloseFullscreen";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import SendIcon from '@mui/icons-material/Send';
-let comments=[]
-const MytweetList = (props)=> {
-  let[comment,setComments]=useState(comments)
-  let[showComment,setShowComment]=useState(false);
-  let[Reply,setReply]=useState('');
-  const location=useLocation();
-  console.log("Location",location)
-  const hideShowComment=()=>setShowComment(false);
-  let buttondiable=false
-    if(props.tweet.username===location.state.userName){
-      buttondiable= false;
-    }
-    else{
-      buttondiable= true;
-    }
-    function DeletHandler(){
-        console.log(props.tweet.id);
-        props.deletTweet(props.tweet.username,props.tweet.id)
-    }
-    function UpdateHandler()
-    {
-      props.updateTweet(props.tweet.username,props.tweet.id,props.tweet.descip)
-    }
-    function CommentHandler()
-    {
-      fetch(`getallreply/${props.tweet.id}`,{
-          method: 'GET',
-      }).then(res=>{
-        console.log(res)
-        return res.json()
-      }).then(data=>{
-        comment=data;
-        setComments(comment);
+import SendIcon from "@mui/icons-material/Send";
+let comments = [];
+const MytweetList = (props) => {
+  let [comment, setComments] = useState(comments);
+  let [showComment, setShowComment] = useState(false);
+  let [Reply, setReply] = useState("");
+  let [likeButton, setlikeButon] = useState("primary");
+  let [likedisable, setlikeDisable] = useState(false);
+  let [likecount, setlikecount] = useState(0);
+  const location = useLocation();
+  console.log("Location", location);
+  const hideShowComment = () => setShowComment(false);
+  let buttondiable = false;
+  if (props.tweet.username === location.state.userName) {
+    buttondiable = false;
+  } else {
+    buttondiable = true;
+  }
+  function DeletHandler() {
+    console.log(props.tweet.id);
+    props.deletTweet(props.tweet.username, props.tweet.id);
+  }
+  function UpdateHandler() {
+    props.updateTweet(props.tweet.username, props.tweet.id, props.tweet.descip);
+  }
+  function CommentHandler() {
+    fetch(`getallreply/${props.tweet.id}`, {
+      method: "GET",
+    })
+      .then((res) => {
+        console.log(res);
+        return res.json();
       })
-      setShowComment(true)
-      console.log(comment)
-    }
-    function replyHandler(e){
-      e.preventDefault()
-      fetch(`${location.state.userName}/reply/${props.tweet.id}`,{
-        method:"POST",
-        headers: { 
-            "Content-Type": "application/json",
-            'Authorization':`Bearer ${localStorage.getItem("jwt")}`
-        },
-          body: JSON.stringify({
-            reply:Reply
-        })
-        }).then((res)=>{
-          if(res.status==200)
-          {
-            CommentHandler();
-          }
-        })
+      .then((data) => {
+        comment = data;
+        setComments(comment);
+      });
+    setShowComment(true);
+    console.log(comment);
+  }
+  function replyHandler(e) {
+    e.preventDefault();
+    fetch(`${location.state.userName}/reply/${props.tweet.id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+      body: JSON.stringify({
+        reply: Reply,
+      }),
+    }).then((res) => {
+      if (res.status == 200) {
+        CommentHandler();
       }
-    return(
-        <div>
-        <Container>
-            <Card>
-                    <Card.Body>
-                            <>
-                                <FaUserSecret className='m-1' />
-                                        <b>{props.tweet.username}</b>
-                            </>
-                            <Card.Text key={props.tweet.id}>{props.tweet.descip}</Card.Text>
-                    </Card.Body>
-                        <Card.Footer>Tweet Post Time :{props.tweet.date}</Card.Footer>
-                        <Box
-                        sx={{
-                          display: 'flex',
-                          flexDirection: 'column',
-                          alignItems: 'center',
-                          '& > *': {
-                            m: 1,
-                          },
-                        }}
-                      >
-                        <ButtonGroup variant="outlined" aria-label="outlined button group">
-                        <Button color='warning' onClick={DeletHandler} disabled={buttondiable}> <DeleteForeverIcon/></Button>
-                        <Button onClick={UpdateHandler} disabled={buttondiable}>Update your Tweet!! </Button>
-                        <Button onClick={CommentHandler} ><CommentRoundedIcon/></Button>
-                        <Fab disabled aria-label="like">
-                            <FavoriteIcon />
-                        </Fab>
-                      </ButtonGroup>
-                      </Box>
-            </Card>
-            <Modal show={showComment}>
-            <ModalHeader>
-            <Button onClick={hideShowComment}><CloseFullscreenIcon/></Button>
-            </ModalHeader>
-            <ModalBody>
-              <Table>
-                                <thead>
-                                    <tr>
-                                        <th>UserName</th>
-                                        <th>Reply</th>
-                                        <th>Time Published</th>
-                                    </tr>
-                                </thead>
-                                {
-                                    (comment.map((reply, k) =>
-                                        < tr >
-                                            <td>{reply.userName}</td>
-                                            <td>{reply.reply}</td>
-                                            <td>{reply.date}</td>
-                                        </tr>
-                                    ))
-                                }
-                </Table>
-            </ModalBody>
-            <ModalFooter>
-            <input type="text" placeholder="Tweet your reply" onChange={e => setReply(e.target.value)}/>
-            <Button onClick={replyHandler}>Reply<SendIcon/></Button>
-            </ModalFooter>
-            </Modal>
-        </Container>
-        </div>
-    );
-}
+    });
+  }
+
+  //Handle Like
+  function handleLike(e) {
+    e.preventDefault();
+    fetch(`${props.tweet.username}/like/${props.tweet.id}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    }).then((res)=>{
+      if(res.status===200)
+      {
+        fetchallLikes();
+        setlikeButon("error");
+        setlikeDisable(true);
+      }
+    })
+  }
+
+
+  //Fetch All Like 
+  function fetchallLikes() {
+    fetch(`getallLike/${props.tweet.id}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        likecount = data;
+        setlikecount(likecount);
+      });
+  }
+  useEffect(() => {
+    fetchallLikes();
+  }, []);
+  return (
+    <div>
+      <Container>
+        <Card>
+          <Card.Body>
+            <>
+              <FaUserSecret className="m-1" />
+              <b>{props.tweet.username}</b>
+            </>
+            <Card.Text key={props.tweet.id}>{props.tweet.descip}</Card.Text>
+          </Card.Body>
+          <Card.Footer>Tweet Post Time :{props.tweet.date}</Card.Footer>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              "& > *": {
+                m: 1,
+              },
+            }}
+          >
+            <ButtonGroup variant="outlined" aria-label="outlined button group">
+              <Button
+                color="warning"
+                onClick={DeletHandler}
+                disabled={buttondiable}
+              >
+                {" "}
+                <DeleteForeverIcon />
+              </Button>
+              <Button onClick={UpdateHandler} disabled={buttondiable}>
+                Update your Tweet!!{" "}
+              </Button>
+              <Button onClick={CommentHandler}>
+                <CommentRoundedIcon />
+              </Button>
+            </ButtonGroup>
+            <Fab
+              aria-label="like"
+              size="small"
+              onClick={handleLike}
+              disabled={likedisable}
+            >
+              <FavoriteIcon color={likeButton}></FavoriteIcon>
+              {likecount}
+            </Fab>
+          </Box>
+        </Card>
+        <Modal show={showComment}>
+          <ModalHeader>
+            <Button onClick={hideShowComment}>
+              <CloseFullscreenIcon />
+            </Button>
+          </ModalHeader>
+          <ModalBody>
+            <Table>
+              <thead>
+                <tr>
+                  <th>UserName</th>
+                  <th>Reply</th>
+                  <th>Time Published</th>
+                </tr>
+              </thead>
+              {comment.map((reply, k) => (
+                <tr>
+                  <td>{reply.userName}</td>
+                  <td>{reply.reply}</td>
+                  <td>{reply.date}</td>
+                </tr>
+              ))}
+            </Table>
+          </ModalBody>
+          <ModalFooter>
+            <input
+              type="text"
+              placeholder="Tweet your reply"
+              onChange={(e) => setReply(e.target.value)}
+            />
+            <Button onClick={replyHandler}>
+              Reply
+              <SendIcon />
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </Container>
+    </div>
+  );
+};
 export default MytweetList;
