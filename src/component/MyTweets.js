0a99032b4ee74ecb7867c0cc665bc55fa12d3a8c
samir281 <samalam281@gmail.com>
2022-08-14 18:31:53 +0530

    All done

--- src/component/MyTweets.js
+++ src/component/MyTweets.js
@@ -5,15 +5,27 @@ let tweets = []
 let dummy = []
 const MyTweets = (props) => {
     let navigate=useNavigate();
-    function onDeletehandler(name,tweetId)
+    function onDeletehandler(name,tweetid)
     {
         console.log(name);
-        console.log(tweetId);
-        fetch(`${name}/delete/${tweetId}`,{
-        method: 'DELETE',
-        headers: { 
-        'Authorization':`Bearer ${localStorage.getItem("jwt")}` 
-        },
+        console.log(tweetid);
+        let form={name,tweetid}
+        // fetch(`${name}/delete/${tweetId}`,{
+        // method: 'DELETE',
+        // headers: { 
+        // 'Authorization':`Bearer ${localStorage.getItem("jwt")}` 
+        // },
+        // })
+        fetch(`/publish`,{
+            method:'POST',
+            headers:{
+                'Authorization':`Bearer ${localStorage.getItem("jwt")}`
+                ,'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                username: name,
+                tweetId: tweetid
+            })
         })
         .then(res => {
             console.log(res.status);
