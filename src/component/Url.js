b26f3519b9e85e5dd2ea622d1c50f1ab8149ee08
samir281 <sa7658241@gmail.com>
2022-08-25 22:14:08 +0530

    some changes

--- src/component/Url.js
+++ src/component/Url.js
@@ -1,1 +1,2 @@
-export const url='http://tweetapplication-env.eba-5smavibf.us-west-2.elasticbeanstalk.com/api/v1.0/tweets'
+// export const url='http://tweetapplication-env.eba-5smavibf.us-west-2.elasticbeanstalk.com/api/v1.0/tweets'
+export const url='https://cors-everywhere.herokuapp.com/http://tweetapplication-env.eba-5smavibf.us-west-2.elasticbeanstalk.com/api/v1.0/tweets'
