7cfd2e0670680cc824b865ac0efa9c3757c6ed7d
samir281 <sa7658241@gmail.com>
2022-08-25 17:56:19 +0530

    some changes1

--- src/component/DirectLogin.js
+++ src/component/DirectLogin.js
@@ -14,7 +14,7 @@ export default function DirectLogin() {
         if(username!=='')
         {
         let form={username,password}
-        fetch(`/login`,{
+        fetch(`${url}/login`,{
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(form)
